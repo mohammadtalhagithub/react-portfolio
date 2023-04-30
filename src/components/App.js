1294a27cf0a2ebb572092f2aa8c1770b850bc65f
mohammadtalhagithub <md.talha226@gmail.com>
2023-05-01 00:08:30 +0530

    Added Navbar.jsx and App.js inside components folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-// import './App.css';
-import Navbar from './components/Navbar';
+// import Navbar from '../src/components/Navbar';
+// import Footer from '../src/components/Footer';
+import Navbar from './Navbar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
-        
+        {/* <Footer /> */}
       </header>
     </div>
   );
