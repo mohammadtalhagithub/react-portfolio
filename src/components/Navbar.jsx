74b851a818e896103c9c752645d19b314e24cc22
mohammadtalhagithub <md.talha226@gmail.com>
2023-04-30 20:14:00 +0530

    Added Header, Navbar, Main, Footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,41 @@
-import React from 'react'
+import React from "react";
+
+function Navbar(props) {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary navinavi">
+                <div class="container-fluid navi">
+                    <a class="navbar-brand" href="#">My Portfolio</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contact</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Projects
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Desktop App Development</a></li>
+                                    <li><a class="dropdown-item" href="#">Web Design</a></li>
+                                    <li><a class="dropdown-item" href="#">3D Printing & CAD</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
