9a926512252414700818714a202b5ecd107b9f78
mohammadtalhagithub <md.talha226@gmail.com>
2023-04-30 20:18:18 +0530

    Added data.js

---
+++ src/components/data/data.js
@@ -0,0 +1,24 @@
+const projects = [
+    {
+        projectTitle: "Desktop App Development",
+        img: "https://www.goodtherapy.org/blog/blog/wp-content/uploads/2014/12/man-with-head-down-300x300.jpg",
+        projectDesc: "Windows application built on .NET framework by using C# programming language."
+    },
+    {
+        projectTitle: "Web Page Development",
+        img: "https://www.goodtherapy.org/blog/blog/wp-content/uploads/2014/12/man-with-head-down-300x300.jpg",
+        projectDesc: "Development of web pages using HTML, CSS, JavaScript, Bootstrap and React."
+    },
+    {
+        projectTitle: "CAD",
+        img: "https://www.goodtherapy.org/blog/blog/wp-content/uploads/2014/12/man-with-head-down-300x300.jpg",
+        projectDesc: "Computer Aided Design using Solidworks & AutoCAD."
+    },
+    {
+        projectTitle: "3D Printing",
+        img: "https://www.goodtherapy.org/blog/blog/wp-content/uploads/2014/12/man-with-head-down-300x300.jpg",
+        projectDesc: "Design & development of prototypes by 3D printer for rapid prototyping."
+    }
+];
+
+export default projects;
